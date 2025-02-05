d8f5183ddca40ec17059c1b1e8457c96cf19661d
J. Nugen <joseph.nuyenp@gmail.com>
2025-02-05 14:15:32 -0500

    Added Weather() - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,16 @@
-# Programmer: Joseph
-# Branch: Weather
-# Date:
-# Program:
+print("\n*****************************************\n")
+
+print("Weather Branch - Developer: Joseph Nuyen\n")
+
+#Import Libraries Here!
+import random
+from time import sleep
+
+
+# Weather Function to determine the weather
+def weather():
+    weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny", "Nagasaki"]
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+
+print (weather())
