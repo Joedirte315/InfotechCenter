79dc2e7daa9be8934252293fe05cd5764c664153
J. Nugen <joseph.nuyenp@gmail.com>
2025-02-11 13:46:47 -0500

    Added sleep and engaged VRS system for Speed

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,8 +1,6 @@
 # Print a decorative separator line
 print("\n*****************************************\n")
-
-# Print the Weather Branch developer information
-print("Weather Branch - Developer: Joseph Nuyen\n")
+print("Weather Branch - Developer: Joseph Nuyen")
 
 # Import necessary libraries
 import random  # For generating random weather conditions
@@ -22,34 +20,48 @@ weatherAlert = weather()
 # Function to handle vehicle response based on the weather condition
 def vehiclesResponseSystem():
     # Check for different weather conditions and respond accordingly
+    
     if weatherAlert == "snowing":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecast of the ", weatherAlert , " weather condition.")
-    
+        sleep(1)
+        print("WRS has been engaged only allowing us to drive 55mph.")
     # Special case for the "Nagasaki" condition (could be a humorous or placeholder entry)
+
     elif weatherAlert == "Nagasaki":
         print("\nYou're probably gonna die because it's", weatherAlert , "2.0")
-
+        sleep(1)
+        print("WRS has been engaged only allowing us to drive 55mph.")
     # Handle other common weather conditions in a similar manner
+
     elif weatherAlert == "blizzard":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecast of the ", weatherAlert , " weather condition.")
-    
+        sleep(1)
+        print("WRS has been engaged only allowing us to drive 55mph.")
+
     elif weatherAlert == "icy":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecast of the ", weatherAlert , " weather condition.")
-    
+        sleep(1)
+        print("WRS has been engaged only allowing us to drive 55mph.")
+
     elif weatherAlert == "rainy":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecast of the ", weatherAlert , " weather condition.")
-    
+        sleep(1)
+        print("WRS has been engaged only allowing us to drive 55mph.")
+
     elif weatherAlert == "windy":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecast of the ", weatherAlert , " weather condition.")
-    
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 55mph.")
+
     elif weatherAlert == "sunny":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecast of the ", weatherAlert , " weather condition.")
-
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 55mph.")
 # Call the function to execute the vehicle response based on the weather condition
 vehiclesResponseSystem()
