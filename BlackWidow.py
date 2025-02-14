c0f56acaa693ed0bbbaa3c7fa0136e8d8916731a
Joedirte315 <joseph.nuyenp@gmail.com>
2025-02-14 13:53:35 -0500

    Added Libraries & gasLevelGaugeFunction - NOT TESTED - Programmed in GitHub

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,2 +1,10 @@
 print("\n********************************************************\n")
 print("Gasoline Branch - Developer: Joseph Nuyen\n")
+
+import random
+from time import sleep
+
+def gasLevelGauge():
+  gasLevelGauge["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
+  return random.choice(gasLevelList)
+  
